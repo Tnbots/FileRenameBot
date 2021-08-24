a796223d9ebd671e2b104268f0aebfdeecb1c86b
Tnbots <89348860+Tnbots@users.noreply.github.com>
2021-08-24 18:48:45 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -26,7 +26,16 @@ class Translation(object):
     NO_THUMB_FOUND = "No Thumbnail found"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Reports @Im_professorr If You Found Meh Useful</b>"""
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Rename Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Im_professorr">Im_professorr</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Support Group</b> : <i><a href="https://t.me/tn_support">Tn_support</a></i>
+"""
     HELP_USER = """Checkout The Available Commands Here \n\n Do Follow  @TN_Bots If You Find This Bot Usefull</b> \n❤️"""
     RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
