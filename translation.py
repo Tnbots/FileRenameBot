25e397e313744a01ea6fb649fd44b2d2a94bc554
Tnbots <89348860+Tnbots@users.noreply.github.com>
2021-08-24 19:04:11 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -26,10 +26,7 @@ class Translation(object):
     NO_THUMB_FOUND = "No Thumbnail found"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @No_OnE_Kn0wS_Me If You Found Meh Useful</b>"""
-    Ohh You Want Help?? 😅
-    
-1.🔹 Send me any Telegram File.\n 2.🔹 Reply to that message with /rename New file name.extension. \n 3.🔹  Send A Photo to make it as permanent Thumbnail \n 4.🔹  I Can Also Convert Files Into Video through /c2v Command \n 5.🔹  Send Me Any Zee5 Url And I'll Upload It Into Telegram 
+    ABOUT_ME = """<b>Meh : \n 1.🔹 Send me any Telegram File.\n 2.🔹 Reply to that message with /rename New file name.extension. \n 3.🔹  Send A Photo to make it as permanent Thumbnail \n 4.🔹  I Can Also Convert Files Into Video through /c2v Command \n 5.🔹  Send Me Any Zee5 Url And I'll Upload It Into Telegram </b>"""1 
     RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
